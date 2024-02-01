9841c0a055e091d03c52af90369f4d48ea27d79d
Nick DeRobertis <whoopnip@gmail.com>
2024-02-01 16:45:56 -0600

    feat: Add support for opening images

--- playwright_trace_browser/app.py
+++ playwright_trace_browser/app.py
@@ -3,11 +3,13 @@ from pathlib import Path
 
 from textual.app import App, ComposeResult
 from textual.containers import Container
-from textual.reactive import var
 from textual.widgets import DirectoryTree, Footer, Header
 
 from playwright_trace_browser._folder import create_restructured_temp_dir_for_viewing
+from playwright_trace_browser._image import open_image
 from playwright_trace_browser._viewer import open_trace_viewer
+
+placeholder_image_path = Path(__file__).parent / "placeholder.jpg"
 
 
 class PlaywrightTraceBrowser(App):
@@ -15,23 +17,19 @@ class PlaywrightTraceBrowser(App):
 
     CSS_PATH = "app.tcss"
     BINDINGS = [
-        ("f", "toggle_files", "Toggle Files"),
         ("q", "quit", "Quit"),
     ]
 
-    show_tree = var(True)
-
-    def watch_show_tree(self, show_tree: bool) -> None:
-        """Called when show_tree is modified."""
-        self.set_class(show_tree, "-show-tree")
+    def __init__(self) -> None:
+        super().__init__()
+        path = "./" if len(sys.argv) < 2 else sys.argv[1]
+        self.path = create_restructured_temp_dir_for_viewing(Path(path))
 
     def compose(self) -> ComposeResult:
         """Compose our UI."""
-        path = "./" if len(sys.argv) < 2 else sys.argv[1]
-        temp_path = create_restructured_temp_dir_for_viewing(Path(path))
         yield Header()
         with Container():
-            yield DirectoryTree(temp_path, id="tree-view")
+            yield DirectoryTree(self.path, id="tree-view")
         yield Footer()
 
     def on_mount(self) -> None:
@@ -42,15 +40,16 @@ class PlaywrightTraceBrowser(App):
     ) -> None:
         """Called when the user click a file in the directory tree."""
         event.stop()
-        # await open_trace_viewer(event.path)
-        self.run_worker(self.open_trace_viewer(event.path))
+        if event.path.suffix == ".zip":
+            self.run_worker(self.open_trace_viewer(event.path))
+        else:
+            self.run_worker(self.open_image(event.path))
 
     async def open_trace_viewer(self, path: Path) -> None:
         await open_trace_viewer(path)
 
-    def action_toggle_files(self) -> None:
-        """Called in response to key binding."""
-        self.show_tree = not self.show_tree
+    async def open_image(self, path: Path) -> None:
+        await open_image(path)
 
 
 if __name__ == "__main__":
